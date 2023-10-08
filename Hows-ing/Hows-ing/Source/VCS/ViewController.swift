fbceee525a5cb4b6ea5fdfc92396ffd98f5ca0dd
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2023-10-08 15:51:01 +0900

    Add assets, temp commit

--- Hows-ing/Hows-ing/Source/VCS/ViewController.swift
+++ Hows-ing/Hows-ing/Source/VCS/ViewController.swift
@@ -7,12 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    @IBOutlet weak var Banner: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        Init()
+        Banner.delegate = self
+        Banner.dataSource = self
     }
+    
+    func Init(){
+        Banner.layer.cornerRadius = 10
+    }
+
 
 
 }
