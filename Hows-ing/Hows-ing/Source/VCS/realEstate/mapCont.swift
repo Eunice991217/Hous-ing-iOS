4f608e41a81418a094fe28c8757b1055718baef5
Eunice991217 <101406317+Eunice991217@users.noreply.github.com>
2023-10-09 02:29:10 +0900

    feat : 부동산 세부 정보 UI

--- Hows-ing/Hows-ing/Source/VCS/realEstate/mapCont.swift
+++ Hows-ing/Hows-ing/Source/VCS/realEstate/mapCont.swift
@@ -12,6 +12,9 @@ class mapCont: UIViewController, CLLocationManagerDelegate{
     
     var longitude_HVC = 0.0
     var latitude_HVC = 0.0
+    
+    var isToggled = false
+    var infoView: UIView?
 
     @IBOutlet weak var mapView: NMFMapView!
     
@@ -40,6 +43,91 @@ class mapCont: UIViewController, CLLocationManagerDelegate{
         let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: latitude, lng: longitude), zoomTo: 15.0)
         mapView.moveCamera(cameraUpdate)
         cameraUpdate.animation = .easeIn
+        
+        let new_marker = NMFMarker()
+        
+        let storyboard: UIStoryboard? = UIStoryboard(name: "realEstate", bundle: Bundle.main)
+        guard let mapDetailCont = storyboard?.instantiateViewController(identifier: "mapDetailCont") else {
+            return
+        }
+        mapDetailCont.modalPresentationStyle = .overFullScreen
+        mapDetailCont.modalTransitionStyle = .crossDissolve
+        
+        // 마커 위치
+        new_marker.position = NMGLatLng(lat: latitude, lng:longitude)
+        new_marker.iconImage = NMFOverlayImage(name: "markerIcon")
+        
+        new_marker.touchHandler = { [weak self] (overlay: NMFOverlay) -> Bool in
+            guard let self = self else { return false }
+            
+            // 토글 상태 변경
+            self.isToggled.toggle()
+            
+            // 토글 상태에 따라 이미지 변경
+            if self.isToggled {
+                new_marker.iconImage = NMFOverlayImage(name: "markerDidTap")
+                // 뷰 생성 및 위치 설정
+                let viewWidth: CGFloat = 60
+                let viewHeight: CGFloat = 50
+                let markerPosition = self.mapView.projection.point(from: new_marker.position)
+                let infoView = UIView(frame: CGRect(x: markerPosition.x - (viewWidth / 2), y: markerPosition.y - viewHeight - 80, width: viewWidth, height: viewHeight))
+                
+                infoView.backgroundColor = UIColor.white
+                infoView.layer.borderColor = UIColor(red: 63/255, green: 130/255, blue: 247/255, alpha: 1).cgColor
+                infoView.layer.borderWidth = 2
+                infoView.layer.cornerRadius = 5
+                
+                let stackView = UIStackView()
+                stackView.axis = .vertical // 수직으로 배치
+                stackView.alignment = .center // 중앙 정렬
+                stackView.distribution = .fill // 두 레이블 사이에 공간을 균등하게 분배
+                stackView.layoutMargins = UIEdgeInsets(top: 5, left: 0, bottom: 5, right: 0) // StackView의 상단과 하단 여백을 10으로 설정
+                stackView.isLayoutMarginsRelativeArrangement = true
+
+                // 첫 번째 레이블 (label1)
+                let label1 = UILabel()
+                label1.text = "13평"
+                label1.textColor = UIColor(red: 153/255, green: 153/255, blue: 153/255, alpha: 1)
+                label1.textAlignment = .center
+                label1.font = UIFont(name: "Pretendard-Bold", size: 12)
+                stackView.addArrangedSubview(label1) // StackView에 추가
+
+                // 두 번째 레이블 (label2)
+                let label2 = UILabel()
+                label2.text = "1.3억"
+                label2.textColor = UIColor(red: 63/255, green: 130/255, blue: 247/255, alpha: 1)
+                label2.textAlignment = .center
+                label2.font = UIFont(name: "Pretendard-Bold", size: 15)
+                stackView.addArrangedSubview(label2) // StackView에 추가
+
+                // StackView의 크기 및 위치 설정
+                stackView.frame = CGRect(x: 0, y: 0, width: viewWidth, height: viewHeight)
+                stackView.spacing = 5 // 레이블 사이의 간격 설정
+
+                // infoView에 StackView 추가
+                infoView.addSubview(stackView)
+                
+                self.infoView = infoView
+                
+                // 뷰를 지도에 추가
+                self.mapView.addSubview(infoView)
+                
+                
+                if let mapDetailCont = mapDetailCont as? mapDetailCont {
+                    
+                    self.present(mapDetailCont, animated: true)
+                }
+                
+            } else {
+                new_marker.iconImage = NMFOverlayImage(name: "markerIcon")
+                // 이미 뷰가 표시 중이면 숨김
+                infoView?.removeFromSuperview()
+            }
+            
+            return true
+        }
+
+        new_marker.mapView = mapView
         
         print(latitude)
         print(longitude)
