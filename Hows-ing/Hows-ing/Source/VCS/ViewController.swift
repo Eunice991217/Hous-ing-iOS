1188cd5a22e93012f25ca7a5c7af7b0b4a81f6b8
Jaesang <59428259+jxx-sx@users.noreply.github.com>
2023-10-08 16:50:03 +0900

    Make Tabbar

--- Hows-ing/Hows-ing/Source/VCS/ViewController.swift
+++ Hows-ing/Hows-ing/Source/VCS/ViewController.swift
@@ -8,15 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
     @IBOutlet weak var Banner: UICollectionView!
+    @IBOutlet weak var TabbarView: UIView!
+    @IBOutlet weak var PaperIcon:  UIImageView!
+    @IBOutlet weak var HomeIcon: UIImageView!
+    @IBOutlet weak var PaperText: UILabel!
+    @IBOutlet weak var HomeText: UILabel!
+    let pageLock = NSLock()
+    var tapNum: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,9 +27,47 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func Init(){
         Banner.layer.cornerRadius = 10
+        TabbarView.layer.cornerRadius = 40
+        TabbarView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        TabbarView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        TabbarView.layer.shadowColor = UIColor.black.cgColor
+        TabbarView.layer.shadowOpacity = 0.2
+        TabbarView.layer.shadowRadius = 24 / UIScreen.main.scale
+        
     }
-
-
-
+    
+    @IBAction func PaperButtonTap(_ sender: Any) {
+        if tapNum == 0 {
+            return
+        }
+        self.pageLock.lock()
+        defer { self.pageLock.unlock() }
+        PaperIcon.image = UIImage(named: "PaperSelect")
+        HomeIcon.image = UIImage(named: "Home")
+        PaperText.textColor = UIColor(named: "HousingBlue")
+        HomeText.textColor = UIColor(named: "HousingGray")
+        tapNum = 0
+    }
+    
+    @IBAction func HomeButtonTap(_ sender: Any) {
+        if tapNum == 1 {
+            return
+        }
+        self.pageLock.lock()
+        defer { self.pageLock.unlock() }
+        PaperIcon.image = UIImage(named: "Paper")
+        HomeIcon.image = UIImage(named: "HomeSelect")
+        PaperText.textColor = UIColor(named: "HousingGray")
+        HomeText.textColor = UIColor(named: "HousingBlue")
+        tapNum = 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
 }
 
